ef79edd8ba9d0638b05b1e682c0144fc23d6b703
David Ackerman <david.w.ackerman@gmail.com>
2011-03-27 17:32:26 -0700

    added js to populate the big window whenever you click on one of the smaller windows

---
+++ github-projects.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".project").click(function(event){
+		var viewer_parent = $("#viewer");
+		var viewer = $("#viewer_content");
+		viewer.empty();
+		viewer.append($(this).html());
+		viewer_parent.css('background-color',
+		  $(this).css('background-color'));
+		event.preventDefault();
+	});
+});
